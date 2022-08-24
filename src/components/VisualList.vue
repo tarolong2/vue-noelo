5b5b2ee132e76e721d2b082988055479daba43cc
tarolong2 <tarolong2@gmail.com>
2022-08-24 13:40:50 +0900

    프로젝트초기화

---
+++ src/components/VisualList.vue
@@ -0,0 +1,35 @@
+<template>
+   <a v-bind:href="vlink">
+      <div 
+        class="visual-img bg"
+        :style="{backgroundImage:'url(' + require(`@/assets/images/${vimg}`) + ')'}"
+      ></div>
+      <div class="visual-txt">
+        <h3 class="visual-title">{{vtitle}}</h3>
+        <span class="visual-desc" v-html="vtxt"></span>
+        <span class="visual-bt">
+          {{vbt}}
+        </span>
+      </div>
+  </a>
+</template>
+
+<script>
+export default {
+  props: ['vimg', 'vtitle', 'vtxt', 'vbt', 'vlink'],
+  setup() {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+
+.bg {
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+
+</style>
